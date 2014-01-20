bcc282bf5968ce802082974d7e4ad0f573b2f599
dolfinsbizou <jorandon@gmail.com>
2014-01-20 18:07:30 +0100

    First commit

---
+++ Paper Plane/Paper Plane/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Paper Plane
+//
+//  Created by Dolf Rattmann on 20/01/2014.
+//  Copyright (c) 2014 Dolf Rattmann. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
