faeb71b75df235ff409147e555662528cfc8d2c5
Guillaume Jorandon <guillaume.jorandon@cea.fr>
2017-09-20 12:32:17 +0200

    Fix warnings for BackgroundGenerator

--- src/BackgroundGenerator.h
+++ src/BackgroundGenerator.h
@@ -32,8 +32,8 @@ private:
     Tilemap m_tm; // La tilemap que génère le générateur
     unsigned int m_currLine; // Le nombre actuel de lignes
     bool m_isWindowBeingCreated; // vaut vrai si une fenêtre de décor est en cours de création
-    int m_windowPos; //valeur de la colonne dans laquelle se situe le coin supérieur gauche de la fenêtre de décoration
     int m_windowLine; //ligne de fenêtre en cours de génération (1: haut, 2: milieu, 3:bas)
+    int m_windowPos; //valeur de la colonne dans laquelle se situe le coin supérieur gauche de la fenêtre de décoration
     int m_lineSinceLastWindow; //nombre de lignes écoulées depuis la dernière fenêtre;
 };
 
